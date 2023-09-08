1d048e4263d4cf4aaf7754db0c52e29fd170526a
Shiraz Yom Tov <shirazyomtov@gmail.com>
2023-09-08 09:36:16 +0300

    Add server.ino

---
+++ carServer2.ino
@@ -0,0 +1,220 @@
+#include "server.h"
+
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "fb_gfx.h"
+#include "soc/soc.h" //disable brownout problems
+#include "soc/rtc_cntl_reg.h"  //disable brownout problems
+#include "esp_http_server.h"
+#include "esp_camera.h"
+
+
+//Replace with your network credentials
+const char* ssid = "Yaniv";
+const char* password = "yaniv123";
+
+// This project was tested with the AI Thinker Model, M5STACK PSRAM Model and M5STACK WITHOUT PSRAM
+#define CAMERA_MODEL_AI_THINKER
+//#define CAMERA_MODEL_M5STACK_PSRAM
+//#define CAMERA_MODEL_M5STACK_WITHOUT_PSRAM
+// Not tested with this model
+//#define CAMERA_MODEL_WROVER_KIT
+#if defined(CAMERA_MODEL_WROVER_KIT)
+  #define PWDN_GPIO_NUM    -1
+  #define RESET_GPIO_NUM   -1
+  #define XCLK_GPIO_NUM    21
+  #define SIOD_GPIO_NUM    26
+  #define SIOC_GPIO_NUM    27
+  
+  #define Y9_GPIO_NUM      35
+  #define Y8_GPIO_NUM      34
+  #define Y7_GPIO_NUM      39
+  #define Y6_GPIO_NUM      36
+  #define Y5_GPIO_NUM      19
+  #define Y4_GPIO_NUM      18
+  #define Y3_GPIO_NUM       5
+  #define Y2_GPIO_NUM       4
+  #define VSYNC_GPIO_NUM   25
+  #define HREF_GPIO_NUM    23
+  #define PCLK_GPIO_NUM    22
+#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     25
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       32
+  #define VSYNC_GPIO_NUM    22
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+#elif defined(CAMERA_MODEL_M5STACK_WITHOUT_PSRAM)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     25
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       17
+  #define VSYNC_GPIO_NUM    22
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+#elif defined(CAMERA_MODEL_AI_THINKER)
+  #define PWDN_GPIO_NUM     32
+  #define RESET_GPIO_NUM    -1
+  #define XCLK_GPIO_NUM      0
+  #define SIOD_GPIO_NUM     26
+  #define SIOC_GPIO_NUM     27
+  
+  #define Y9_GPIO_NUM       35
+  #define Y8_GPIO_NUM       34
+  #define Y7_GPIO_NUM       39
+  #define Y6_GPIO_NUM       36
+  #define Y5_GPIO_NUM       21
+  #define Y4_GPIO_NUM       19
+  #define Y3_GPIO_NUM       18
+  #define Y2_GPIO_NUM        5
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     23
+  #define PCLK_GPIO_NUM     22
+#else
+  #error "Camera model not selected"
+#endif
+
+
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+ 
+  Serial.begin(115200);
+  Serial.setDebugOutput(false);
+  pinMode(gpLb, OUTPUT); //Left Backward
+  pinMode(gpLf, OUTPUT); //Left Forward
+  pinMode(gpRb, OUTPUT); //Right Forward
+  pinMode(gpRf, OUTPUT); //Right Backward
+  pinMode(gpLed, OUTPUT); //Light
+
+  //initialize
+  digitalWrite(gpLb, LOW);
+  digitalWrite(gpLf, LOW);
+  digitalWrite(gpRb, LOW);
+  digitalWrite(gpRf, LOW);
+  digitalWrite(gpLed, LOW);
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  // config.frame_size = FRAMESIZE_SVGA;
+  config.frame_size = FRAMESIZE_CIF;
+  // config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  
+  // if(psramFound()){
+  //   Serial.println("psram found, higher qualiy of image");
+  //   config.frame_size = FRAMESIZE_UXGA;
+  //   config.jpeg_quality = 10;
+  //   config.fb_count = 2;
+  // } else {
+  //   Serial.println("no psram ,lower qualiy of image");
+  //   config.frame_size = FRAMESIZE_SVGA;
+  //   config.jpeg_quality = 12;
+  //   config.fb_count = 1;
+  // }
+  
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  
+  // Wi-Fi connection
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  Serial.print("Camera Capture Ready! Go to: http://");
+  Serial.print(WiFi.localIP());
+
+  // Start streaming web server
+  startCameraServer();
+}
+
+void WheelAct(int nLf, int nLb, int nRf, int nRb){
+ digitalWrite(gpLf, nLf);
+ digitalWrite(gpLb, nLb);
+ digitalWrite(gpRf, nRf);
+ digitalWrite(gpRb, nRb);
+}
+const int cycle = 10;
+
+void loop() {
+  int parton = cycle * carSpeed/100;
+  // Serial.println("speed = " + String(carSpeed) + " direction is " + String(carDirection));
+  switch (carDirection) {
+    case FORWARD:
+      WheelAct(HIGH, LOW, HIGH, LOW);
+      break;
+
+    case BACKWARD:
+      WheelAct(LOW, HIGH, LOW, HIGH);
+      break;
+    
+    case LEFT:
+      WheelAct(HIGH, LOW, LOW, HIGH);
+      break;
+    
+    case RIGHT:
+      WheelAct(LOW, HIGH, HIGH, LOW);
+      // Right turn logic
+      break;
+    
+    default:
+      // Stop the car
+      WheelAct(LOW, LOW, LOW, LOW);
+      break;
+  }
+
+  delay(parton);
+  WheelAct(LOW, LOW, LOW, LOW);
+  delay(cycle - parton);
+}
